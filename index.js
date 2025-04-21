361f96e7ab1925b217db43c16de5a5dd9695d02e
rajaasif <rajaasif023@gmail.com>
2025-04-21 14:04:57 +0500

    w

--- index.js
+++ index.js
@@ -11,6 +11,9 @@ app.use(express.json());
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
+app.get('/asif', (req, res) => {
+  res.send('Hello World!');
+});
 
 app.listen(port, () => {
   console.log(`✅ Server running on http://localhost:${port}`);
