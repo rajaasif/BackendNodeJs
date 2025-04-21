d867c842a11e79b6a90fc434e78f18e0f363aa11
rajaasif <rajaasif023@gmail.com>
2025-04-21 13:55:01 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`✅ Server running on http://localhost:${port}`);
+});
